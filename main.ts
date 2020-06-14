260566419a2e98ed000ecad8d0b2e911b2e33c9c
MHii88 <66858728+MHii88@users.noreply.github.com>
2020-06-14 15:00:57 +0800

    Created third level, added new mechanic known as enemy.

--- main.ts
+++ main.ts
@@ -59,26 +59,23 @@ namespace myTiles {
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonTeal, function (sprite, location) {
     music.pewPew.play()
-    tiles.setTileAt(tiles.getTileLocation(3, 11), sprites.dungeon.buttonTealDepressed)
+    tiles.setTileAt(location, sprites.dungeon.buttonTealDepressed)
     for (let value of tiles.getTilesByType(sprites.dungeon.hazardSpike)) {
         tiles.setTileAt(value, sprites.dungeon.floorDark0)
         tiles.setWallAt(value, false)
     }
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.oceanDepths9, function (sprite, location) {
-    game.over(false)
-})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairWest, function (sprite, location) {
     tiles.setTilemap(tiles.createTilemap(
-            hex`100010001114151514141e151515141415151516091918172d10101010101b191819191c091918182d1010101010191b1919191c20191b1a2d10101010101a191819182421222a2313131313131313131313132700202a240000000000000000000000001129172615151600000000000000000020191b191b2b24000000000000000000202a1819192a25000000000000000000281b2a19191b24000000000000000000282a1b18192c250000000000000000002112121313122700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            hex`100010001114151514141e151515141415151516091918171910101010101b1918192f0909191818191010101010191b19192f0920191b1a1910101010101a1918192f24201b2a17231313131313131313131327202a2a182000000000000000000000002017181b26151600000000000000000020191b191b2b24000000000000000000202a1819192a25000000000000000000281b2a19191b24000000000000000000282a1b18192e250000000000000000002112121313122700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
             img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 . . . . . . . . . . . . . . . 
-2 . . . . . . . . . . . . . . . 
-2 . . . . . . . . . . . . . . 2 
-2 2 . 2 2 2 2 2 2 2 2 2 2 2 2 2 
-. 2 . 2 . . . . . . . . . . . . 
-2 2 . 2 2 2 2 . . . . . . . . . 
+2 . . . . 2 2 2 2 2 . . . . 2 . 
+2 . . . . 2 2 2 2 2 . . . . 2 . 
+2 . . . . 2 2 2 2 2 . . . . 2 2 
+2 . . . 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . 2 . . . . . . . . . . . 
+2 . . . 2 2 2 . . . . . . . . . 
 2 . . . . . 2 . . . . . . . . . 
 2 . . . . . 2 . . . . . . . . . 
 2 . . . . . 2 . . . . . . . . . 
@@ -89,12 +86,77 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairWest, function (spri
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `,
-            [myTiles.tile0,myTiles.tile1,myTiles.tile2,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.floorDark0,sprites.dungeon.floorDark1,sprites.dungeon.stairEast,sprites.dungeon.floorDark3,sprites.dungeon.floorDark5,sprites.builtin.forestTiles13,sprites.builtin.forestTiles9,sprites.builtin.oceanDepths8,sprites.dungeon.hazardHole,sprites.builtin.oceanDepths9,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.floorLight5,sprites.dungeon.floorLight1,sprites.dungeon.floorLight0,sprites.dungeon.floorLight3,sprites.dungeon.floorLightMoss,sprites.dungeon.stairWest,sprites.dungeon.purpleOuterWest2,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.stairLadder,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.floorLight4,sprites.dungeon.buttonOrange,sprites.dungeon.buttonPink,sprites.castle.rock2],
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.floorDark0,sprites.dungeon.floorDark1,sprites.dungeon.stairEast,sprites.dungeon.floorDark3,sprites.dungeon.floorDark5,sprites.builtin.forestTiles13,sprites.builtin.forestTiles9,sprites.builtin.oceanDepths8,sprites.dungeon.hazardHole,sprites.builtin.oceanDepths9,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.floorLight5,sprites.dungeon.floorLight1,sprites.dungeon.floorLight0,sprites.dungeon.floorLight3,sprites.dungeon.floorLightMoss,sprites.dungeon.stairWest,sprites.dungeon.purpleOuterWest2,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.stairLadder,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.floorLight4,sprites.dungeon.buttonOrange,sprites.dungeon.buttonPink,sprites.castle.rock2,sprites.dungeon.buttonTeal,sprites.dungeon.hazardSpike,sprites.dungeon.buttonOrangeDepressed,sprites.dungeon.buttonTealDepressed],
             TileScale.Sixteen
         ))
     tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 2))
     game.splash("Hmm...looks like I need to find a way across")
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairEast, function (sprite, location) {
+    tiles.setTilemap(tiles.createTilemap(
+            hex`100010000000030404050406060504050507000000000c1a12121212121212121408000000000c1214121212181812121209000000000e1212121212121214141209000000000c1a18181214121218161a09000000000c1212121814121218121208000000000e1212121212121212121209000000000c12141218121214121a1b09000000000c1212121814141212181809000000000e1219121814121212181c08000000000e1b19191212121212121c08000000000e1218181212141212121208000000000b100f1d1218121412141c08000000000000000c1212121212121a09000000000000000e12121212121a1508000000000000000b101111100f0f0f0a0000`,
+            img`
+. . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . 2 2 . . . . . . . . . 2 . . 
+. . 2 . . . . . . . . . . 2 . . 
+. . 2 . . . . . . . . . . 2 . . 
+. . 2 2 . . . . . . . . 2 2 . . 
+. . 2 . . . . . . . . . . 2 . . 
+. . 2 . . . . . . . . . . 2 . . 
+. . 2 . . . . . . . . 2 2 2 . . 
+. . 2 . . . . . . . . . . 2 . . 
+. . 2 . 2 . . . . . . . 2 2 . . 
+. . 2 2 2 2 . . . . . . 2 2 . . 
+. . 2 . . . . . . . . . . 2 . . 
+. . 2 2 2 2 . . . . . . 2 2 . . 
+. . . . . 2 . . . . . . 2 2 . . 
+. . . . . 2 . . . . . 2 2 2 . . 
+. . . . . 2 2 2 2 2 2 2 2 2 . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.stairSouth,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.stairNorth,sprites.dungeon.floorDark0,sprites.dungeon.floorDarkDiamond,sprites.dungeon.floorDark1,sprites.dungeon.floorDark3,sprites.dungeon.floorDark5,sprites.dungeon.floorMixed,sprites.dungeon.floorDark4,sprites.castle.rock0,sprites.castle.rock2,sprites.castle.rock1,sprites.builtin.oceanDepths0,sprites.dungeon.greenInnerNorthEast],
+            TileScale.Sixteen
+        ))
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(7, 1))
+    Ghost_1 = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . f f f f . . . . . . . . . . 
+. . . . . . . . f f 1 1 1 1 f f . . . . . . . . 
+. . . . . . . f b 1 1 1 1 1 1 b f . . . . . . . 
+. . . . . . f b d 1 1 1 1 1 1 1 f . . . . . . . 
+. . . . . . f d d 1 1 1 1 1 1 1 d f . . . . . . 
+. . . . . . f d d d 1 1 1 1 1 1 d f . . . . . . 
+. . . . . . f d d d d d d 1 1 1 d f . . . . . . 
+. . . . . . f d d d d d d 1 1 1 d f . . . . . . 
+. . . . . . f b d d d d d d d 1 d f . . . . . . 
+. . . . . . f f b b d d b f d 1 d f . . . . . . 
+. . . . . . . f c b b d c f d d b f . . . . . . 
+. . . . . . . f f f b d d c c f f f f . . . . . 
+. . . . . . . f f f f c f b b b 1 b c f . . . . 
+. . . . . . f f f f f f f f c d 1 b 1 f . . . . 
+. . . f f f f f f f f f f . . f d f d f . . . . 
+. . . . . f f f f f f . . . . . f . f . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+    Ghost_1.setVelocity(50, 0)
+})
+scene.onHitWall(SpriteKind.Enemy, function (sprite) {
+	
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonOrange, function (sprite, location) {
+    tiles.setTileAt(location, sprites.dungeon.buttonOrangeDepressed)
+    music.pewPew.play()
+    for (let value of tiles.getTilesByType(sprites.builtin.oceanDepths9)) {
+        tiles.setTileAt(value, sprites.dungeon.floorLight0)
+        tiles.setWallAt(value, false)
+    }
+})
+let Ghost_1: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundImage(img`
 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
@@ -223,7 +285,7 @@ for (let index = 0; index < 4; index++) {
 }
 music.powerUp.play()
 tiles.setTilemap(tiles.createTilemap(
-            hex`1000100000000e110f110f13110f110f0f10000000001215151515151515151523180000000012151515231515151523232b000000001215152315151515152315180000000012151515152315151523152a0000000012151515152315151523151800000000161717171b2323141a171a1900000000000000001c1515180000000000000000000000002c1515180000000000000000000000001c15152b00000000000000000e11130f1d23151e0f110f10000000001c301515151515151515151e0f0f00001c1515231515151515231524272600001c1515151515152315151524262600001c23151515232323151515141a1a0000161a171a1a1a291a1a171a190000`,
+            hex`1000100000000e110f110f13110f110f0f10000000001215151515151515151523180000000012151515231515151523232b000000001215152315151515152315180000000012151515152315151523152a0000000012151515152315151523151800000000161717171b2323141a171a1900000000000000001c1515180000000000000000000000002c1515180000000000000000000000001c15152b00000000000000000e11130f1d23151e0f110f10000000001c301515151515151515151e0f0f00001c1515231515151515231524262600001c1515151515152315151524262600001c23151515232323151515141a1a0000161a171a1a1a291a1a171a190000`,
             img`
 . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
 . . 2 . . . . . . . . . . 2 . . 
